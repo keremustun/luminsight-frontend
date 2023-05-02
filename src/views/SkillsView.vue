481d547b6af34b296e2574767d3488aa911cccf8
Randy Vroegop <randy.vroegop@luminis.eu>
2023-05-02 15:08:56 +0200

    component for modal

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AddSkillCard from '../components/SkillCard.vue'
+import AddSkillModal from '../components/AddSkillModal.vue'
 </script>
 
 <script>
@@ -29,30 +29,10 @@ export default {
 
   <main>
     <p v-for="f in facts">{{ f.text }}</p>
-    <!--    <AddSkillCard :stars="store.skills['C#'].stars"></AddSkillCard>-->
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Launch demo modal
-    </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <AddSkillModal id="id-1" titleText="cats" bodyText="bodyText" safeButtonText="Save" closeButtonText="Lol" />
   </main>
 </template>
 
