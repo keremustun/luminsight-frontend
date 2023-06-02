70e5e180650e8f3474e1cc63a8fda4c2824b4f80
Kerem Ustun <Kerem.Ustun@luminis.eu>
2023-06-02 17:38:06 +0200

    download pdf

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -25,29 +25,40 @@ export default {
     methods: {
         async generatePDF() {
             try {
-                this.isPdfGeneration = true;
                 const resumeElement = this.$refs.resumeContainer;
                 const htmlContent = resumeElement.innerHTML;
                 const styling = `<style>${this.styling}</style>`;
+
                 const response = await axios.post('https://localhost:7224/api/person/generatePdf', {
                     htmlContent,
                     styling,
+                }, {
+                    responseType: 'blob', // Set response type as blob
                 });
+                // Create a blob URL from the response data
+                const blobUrl = URL.createObjectURL(response.data);
 
-                console.log(response.data); // Handle the response from the backend
+                // Create an anchor element to trigger the file download
+                const link = document.createElement('a');
+                link.href = blobUrl;
+                link.download = 'resume.pdf';
+                link.click();
+
+                // Clean up the blob URL
+                URL.revokeObjectURL(blobUrl);
             } catch (error) {
                 console.error(error); // Handle the error
-            } finally {
-                this.isPdfGeneration = false;
             }
-        },
-    },
+        }
+    }
+
 };
 </script>
   
 <template>
     <div>
         <button @click="generatePDF">Download PDF</button>
+        <a ref="downloadLink" style="display: none;"></a>
     </div>
     <div class="row background">
         <div class="container">
